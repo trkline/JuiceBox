9a21b3c10bd0123615e12ed8208503f275fe0414
trkline <trkline1@gmail.com>
2022-01-09 23:33:06 -0600

    first pt done

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const apiRouter = require('./api');
+const morgan = require('morgan');
+const {client} = require('./db');
 
+client.connect();
 
+server.use(morgan('dev'));
+server.use('/api', apiRouter);
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
